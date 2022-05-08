d1cf277c569804b7af8f3c06746da36aadc6600d
Tsaqif Razin <tsaqifrazin@gmail.com>
2022-05-08 20:56:27 +0800

    fix error

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.use('/static', express.static('/public'))
+app.use('/cssFiles',express.static(path.join(__dirname+ '/assets')));
 
 app.get('/', function(req,res){
-    console.log(path.join(__dirname, '/index.html'))
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname,'/index.html'))
 })
 
-app.listen(8080, '0.0.0.0')
+app.listen(8080)
 console.log("Aplikasi berjalan")
