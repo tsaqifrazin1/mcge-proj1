c35a3d0af378226c4cd447fe285fcf0b6cdc7adf
Tsaqif Razin <tsaqifrazin@gmail.com>
2022-05-08 19:29:59 +0800

    add .yaml file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.use('/static', express.static('public'))
+app.use('/static', express.static('/public'))
 
 app.get('/', function(req,res){
     console.log(path.join(__dirname, '/index.html'))
